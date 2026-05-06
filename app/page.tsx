25c287b9fdb2fc893cf9b44314521f08c88f131f
Eduardo Benavides <elbedev90@gmail.com>
2026-05-06 11:41:48 -0600

    fix: remove overflow-x-hidden from main (clips content vertically on iOS)

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black overflow-x-hidden w-full">
+    <main className="min-h-screen bg-black w-full">
       <Nav />
       <Hero />
       <Marquee />
